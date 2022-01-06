7207e7d8a966208495245ff698e757bed90ecb3c
AUrieta13 <adaiurieta@gmail.com>
2022-01-05 19:21:36 -0600

    revised and edited code.

--- js/app.js
+++ js/app.js
@@ -41,7 +41,7 @@ function updateFilters() {
     // 5. If a filter value was entered then add that filterId and value
     // to the filters list. Otherwise, clear that filter from the filters object.
   if (Valueelement) {
-    filters(Elementid)=Valueelement;
+    filters[Elementid]=Valueelement;
   }
   else {
     delete filters[Elementid];
@@ -60,7 +60,7 @@ function updateFilters() {
     // 9. Loop through all of the filters and keep any data that
     // matches the filter values
     Object.entries(filters).forEach(([key, value]) => {
-      filteredData = filteredData.filter(row => row(key) === value);
+      filteredData = filteredData.filter(row => row[key] === value);
     })
   
     // 10. Finally, rebuild the table using the filtered data
